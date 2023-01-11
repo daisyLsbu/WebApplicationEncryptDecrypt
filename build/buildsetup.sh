83d16ab9af8a7a892f44dfe8f4707fdb7bc76f08
daisyLsbu <mymail.daisy@gmail.com>
2023-01-11 21:59:03 +0000

    build scripts

---
+++ build/buildsetup.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install git
+git clone https://github.com/daisyLsbu/WebApplicationEncryptDecrypt.git
+cd WebApplicationEncryptDecrypt
+sudo apt install python3
+sudo apt install venv
---
+++ build/launch.sh
@@ -0,0 +1,3 @@
+buildsetup.sh
+install.sh
+run.sh
